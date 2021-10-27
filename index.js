12149e29c711b6f0493dddc7ba9160d1106641eb
Artem Tumanov <artem_tuman@mail.com>
2021-10-27 15:57:56 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+import { initializeApp } from "firebase/app";
+import {getMessaging, getToken, onMessage} from "firebase/messaging";
+
+const firebaseApp = initializeApp({
+    apiKey: "AIzaSyAoBAydROv3Qwyn5J3_EutU2XRW8khE8qg",
+    authDomain: "hello-world-b0136.firebaseapp.com",
+    projectId: "hello-world-b0136",
+    storageBucket: "hello-world-b0136.appspot.com",
+    messagingSenderId: "279813137456",
+    appId: "1:279813137456:web:c3a6f0f3ad1227f962f9f5",
+    measurementId: "G-S0NDNX040N"
+});
+
+
+const messaging = getMessaging(firebaseApp);
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/firebase-messaging-sw.js')
+        .then(function(registration) {
+            console.log('Registration successful, scope is:', registration.scope);
+        }).catch(function(err) {
+        console.log('Service worker registration failed, error:', err);
+    });
+}
+
+getToken(messaging, { vapidKey: 'BLUSCDTEaVlDDepz5mnvqWn0vH3kCUQGXjhs4j-u7w-OIYxjvIxuqi9u-GPBk-ilW_pZlWhOwfe8-G3FqXvqlq8' }).then((currentToken) => {
+    console.log(currentToken);
+    if (currentToken) {
+    } else {
+        // Show permission request UI
+        console.log('No registration token available. Request permission to generate one.');
+        // ...
+    }
+}).catch((err) => {
+    console.log('An error occurred while retrieving token. ', err);
+    // ...
+});
+
+onMessage(messaging, (payload) => {
+    alert(payload);
+});
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: "development",
+    entry: "./index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].[hash].js"
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HTMLWebpackPlugin({ template: "./index.html"})
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, ''),
+        },
+        port: 5000
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                    }
+                }
+            }
+        ]
+    }
+};
