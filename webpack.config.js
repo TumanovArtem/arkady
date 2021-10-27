8a66f8e5714875129e44fffd3dc55ebbf259dc77
Artem Tumanov <artem_tuman@mail.com>
2021-10-27 17:41:18 +0300

    fdsfds

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     mode: "development",
     entry: "./index.js",
     output: {
-        path: path.resolve(__dirname, "dist"),
+        path: path.resolve(__dirname, "public"),
         filename: "[name].[hash].js"
     },
     plugins: [
